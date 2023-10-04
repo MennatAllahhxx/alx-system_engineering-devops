7c1763763e7fff524bbf7d90727c3d89eb3850db
Menna Hassan <h.menna85@gmail.com>
2023-10-05 02:49:45 +0300

    updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,7 +3,7 @@
 exec { 'http_header':
        command => 'sudo apt-get -y update;
        sudo apt-get -y install nginx;
-       sudo sed -i "/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; }" /etc/nginx/sites-available/default;
+       sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
        sudo service nginx restart',
        provider => shell
 }
