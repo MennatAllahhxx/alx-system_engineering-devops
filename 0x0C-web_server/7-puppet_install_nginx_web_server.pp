22e7baa9549c0ef8f58079d862ba457bdcb9f006
Menna Hassan <h.menna85@gmail.com>
2023-10-05 03:38:09 +0300

    updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 # install nginx using puppet
-exec { 'nginx install':
-       command => 'sudo apt-get -y update;
-       sudo apt-get -y install nginx;
-       sudo ufw allow 'Nginx HTTP';
-       sudo sh -c "echo \"Hello World!\" > /var/www/html/index.nginx-debian.html";
-       sudo sed -i "rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default;
-       sudo service nginx restart',
-       provider => shell
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
