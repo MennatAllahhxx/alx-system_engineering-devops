9d585148625a6e4a792628c714ab8d4f64906f4b
Menna Hassan <h.menna85@gmail.com>
2023-10-24 03:53:30 +0300

    updated

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""2-export_to_JSON module"""
+
+import json
+import requests
+import sys
+
+
+def emp_info_json():
+    """for a given ID, returns information about progress"""
+    api_url = 'https://jsonplaceholder.typicode.com/users'
+    usr_id = sys.argv[1]
+    response = requests.get(f'{api_url}/{usr_id}').json()
+    tasks = requests.get(f'{api_url}/{usr_id}/todos').json()
+    usr_name = response.get('username')
+
+    tasks_dict = []
+
+    for task in tasks:
+        tasks_dict.append({'task': task.get('title'),
+                           'completed': task.get('completed'),
+                           'username': task.get('username')})
+
+    filename = f'{usr_id}.json'
+
+    with open(filename, mode='w') as f:
+        json.dump({usr_id: tasks_dict}, f)
+
+
+if __name__ == '__main__':
+    emp_info_json()
