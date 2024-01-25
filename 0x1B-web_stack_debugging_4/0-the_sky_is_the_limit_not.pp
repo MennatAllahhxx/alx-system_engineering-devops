03a1b406d751231d516386179433d4cc28bbf2a6
Menna Hassan <h.menna85@gmail.com>
2024-01-26 01:28:25 +0200

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# change amount of files in nginx
+exec { 'fix':
+    provider => shell,
+    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx'
+}
+exec { 'restart':
+    provider => shell,
+    command  => 'sudo service nginx restart'
+}
