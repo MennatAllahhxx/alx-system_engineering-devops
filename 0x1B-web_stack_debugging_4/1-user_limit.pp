de8e055814de5744694c5f848ffa97b019d0f6d6
Menna Hassan <h.menna85@gmail.com>
2024-01-26 01:48:18 +0200

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # login with the holberton user and open a file without any error message
 exec { 'fix-hard-file':
     provider => shell,
-    command  => 'sudo sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf'
+    command  => 'sudo sed -i "s/5/50000/g" /etc/security/limits.conf'
 }
 exec { 'fix-soft-file':
     provider => shell,
-    command  => 'sudo sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf'
+    command  => 'sudo sed -i "s/4/50000/g" /etc/security/limits.conf'
 }
